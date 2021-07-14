e4f03a2fdae2021739ae1799c680114577a41ba7
Yura-Myrosh <72310926+Yura-Myrosh@users.noreply.github.com>
2021-07-14 17:04:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ASCII_art
+{
+    class Program
+    {
+        private const double WIDTH_OFFSET = 1.7;
+        private const int MAX_WIDTH = 474;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Images | *.bmp; *.png; *.jpg; *.JPEG"
+            };
+
+            while (true)
+            {
+                Console.ReadLine();
+
+                if (openFileDialog.ShowDialog() != DialogResult.OK)
+                {
+                    continue;
+                }
+
+                Console.Clear();
+
+                var bitmap = new Bitmap(openFileDialog.FileName);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.ToGrayscale();
+
+                var converter = new BitmapToASCIIConverter(bitmap);
+
+                var rows = converter.Convert();
+
+                foreach (var row in rows)
+                {
+                    Console.WriteLine(row);
+                }
+
+                rows = converter.Convert(true);
+                File.WriteAllLines("image.txt", rows.Select(x => new string(x)));
+                Console.SetCursorPosition(0, 0);
+            }
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            var newHeight = bitmap.Height / WIDTH_OFFSET * MAX_WIDTH / bitmap.Width;
+
+            if (bitmap.Width > MAX_WIDTH || bitmap.Height > newHeight)
+            {
+                bitmap = new Bitmap(bitmap, new Size(MAX_WIDTH, (int)newHeight));
+            }
+
+            return bitmap;
+        }
+    }
+}
